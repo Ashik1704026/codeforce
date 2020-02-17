ca6aac3ea2d812692d8edab618a05f3d62cebabc
Ashik1704026 <ashiqurr343@gmail.com>
2020-02-17 17:12:39 +0600

    ac

--- 1292A.cpp
+++ 1292A.cpp
@@ -1,18 +1,3 @@
-// #include<iostream>
-// using namespace std;
-// int n,q,a[2][100003],f,b,x,y;
-// int main(){
-//     for(cin>>n>>q;q--;){
-//         cin>>x>>y;
-//         b =a[x-1][y-1]?-1:1;
-//         f += b*(a[2-x][y-2]+a[2-x][y-1]+a[2-x][y]);
-//         a[x-1][y-1]^=1;
-//         cout<<f<<" ";
-//         puts(f?"nO":"yEs");
-//     }
-// }
-
-
 
 #include<bits/stdc++.h>
 using namespace std;
