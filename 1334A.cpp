d943862e0bdeb2bb2c542204b2b770d90fe021db
Ashik1704026 <ashiqurr343@gmail.com>
2020-04-11 17:54:57 +0600

    ac

--- 1334A.cpp
+++ 1334A.cpp
@@ -6,33 +6,15 @@ int main(){
     while(t --){
         int n,flag = 1;
         cin >>  n;
-        pair<int,int> ar[n],br[n];
-        int mn = 100000;
-        for(int i = 0;i < n;i ++){
-            cin >>ar[i].first >> ar[i].second;
-            br[i].first = ar[i].first;
-            br[i].second = ar[i].second;
-        }
-        for(int i = 1;i < n;i ++){
-            if(ar[i].first < ar[i-1].first)
+        int a = 0,b = 0;
+        while(n --){
+            int x,y;
+            cin >> x >> y;
+            if(x < a || y < b || x < y || y - a > x - a)
                 flag = 0;
+            a = x,b = y;
         }
-        for(int i = 0;i < n;i ++){
-            if(br[i].second > br[i].first)
-                flag = 0;
-        }
-        for(int i = 1;i < n;i ++){
-            if(br[i].second < br[i-1].second)
-                flag = 0;
-        }
-        for(int i = 1;i < n;i ++){
-            if(br[i].second > br[i-1].second && br[i].first <= br[i-1].first)
-                flag = 0;
-        }
-        for(int i = 1;i < n;i ++){
-            if(br[i].first > br[i-1].first && br[i].second < br[i-1].second)
-                flag = 0;
-        }
+       
     if(flag)
         cout << "YES\n";
     else
